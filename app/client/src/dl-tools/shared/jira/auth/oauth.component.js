34c0002c1ec75c7eb77b27e700ea836e906250fd
Max Gopey <max.gopey@symmetrics.de>
2018-02-17 13:53:10 +0200

    Redirect to current page after login with oauth components.

--- app/client/src/dl-tools/shared/jira/auth/oauth.component.js
+++ app/client/src/dl-tools/shared/jira/auth/oauth.component.js
@@ -1,12 +1,13 @@
 define([], function() {
     return {
         /* @ngInject */
-        controller: function(BACKEND_URL) {
+        controller: function($state, BACKEND_URL) {
             this.auth = function() {
-                const locationOrigin = window.location.origin ||
-                    (window.location.protocol + '//' + window.location.host);
 
-                window.location.href = BACKEND_URL + '/jira-auth/oauth?redirect=' + locationOrigin;
+                let stateToRedirectTo = $state.is('login') ? 'dashboard' : $state.current;
+                let redirectUrl = $state.href(stateToRedirectTo, {}, {absolute: true});
+
+                window.location.href = BACKEND_URL + '/jira-auth/oauth?redirect=' + redirectUrl;
             };
         },
         template: '<button type="button" class="btn btn-primary" ng-click="$ctrl.auth()">Login with Jira (OAuth)</button>'
